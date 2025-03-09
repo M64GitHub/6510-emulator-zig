62342efa9fa7660f06d6a3022848847fae8d780a
M64GitHub <m64.overdrive@gmail.com>
2025-03-09 16:26:17 +0100

    correct asm in comments

--- src/main.zig
+++ src/main.zig
@@ -16,22 +16,22 @@ pub fn main() !void {
     // 0805: 9D 00 D4                    STA $D400,X     ; 5
     // 0808: E8                          INX             ; 2
     // 0809: E0 19                       CPX #$19        ; 2
-    // 080B: D0 F6                       BNE $0804       ; 2/3
+    // 080B: D0 F6                       BNE $0803       ; 2/3
     // 080D: 60                          RTS             ; 6
 
     cpu.WriteByte(0xa9, 0x0800); //         LDA
     cpu.WriteByte(0x0a, 0x0801); //             #0A     ; 10
     cpu.WriteByte(0xaa, 0x0802); //         TAX
-    cpu.WriteByte(0x69, 0x0803); // LOOP:   INX
-    cpu.WriteByte(0x1e, 0x0804); //         CPX
-    cpu.WriteByte(0x9d, 0x0805); //             #$14    ; 20
-    cpu.WriteByte(0x00, 0x0806); //         BNE
-    cpu.WriteByte(0xd4, 0x0807); //             LOOP
-    cpu.WriteByte(0xe8, 0x0808); //         RTS
-    cpu.WriteByte(0xe0, 0x0809); //         RTS
-    cpu.WriteByte(0x19, 0x080A); //         RTS
-    cpu.WriteByte(0xd0, 0x080B); //         RTS
-    cpu.WriteByte(0xf6, 0x080C); //         RTS
+    cpu.WriteByte(0x69, 0x0803); //         ADC
+    cpu.WriteByte(0x1e, 0x0804); //             #$1E
+    cpu.WriteByte(0x9d, 0x0805); //         STA $
+    cpu.WriteByte(0x00, 0x0806); //                00
+    cpu.WriteByte(0xd4, 0x0807); //              D4
+    cpu.WriteByte(0xe8, 0x0808); //         INX
+    cpu.WriteByte(0xe0, 0x0809); //         CPX
+    cpu.WriteByte(0x19, 0x080A); //             #19
+    cpu.WriteByte(0xd0, 0x080B); //         BNE
+    cpu.WriteByte(0xf6, 0x080C); //             $0803 (-10)
     cpu.WriteByte(0x60, 0x080D); //         RTS
     cpu.PrintStatus();
 
